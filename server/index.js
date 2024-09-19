a990d3e8c1e762e279301f25eac16c1ff9037098
Vivek Korah <lcb2021051@iiitl.ac.in>
2024-09-19 20:06:43 +0530

    hgfjyfyf

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 });
 
 const io = new Server(server, {
-  cors: { origin: process.env.BACKEND_URL },
+  cors: { origin: process.env.FRONTEND_URL },
   credentials: true,
 });
 
