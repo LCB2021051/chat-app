564da591144025e4a8ee229cf405eba0b6439466
Vivek Korah <lcb2021051@iiitl.ac.in>
2024-09-28 16:19:30 +0530

    trying

--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ require("dotenv").config();
 // CORS configuration
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: [
+      "https://chat-app-client-snowy.vercel.app",
+      "http://localhost:3000",
+    ],
     methods: ["POST", "GET", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -45,7 +48,10 @@ const server = app.listen(process.env.PORT || 5000, () => {
 // Socket.io configuration
 const io = new Server(server, {
   cors: {
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: [
+      "https://chat-app-client-snowy.vercel.app",
+      "http://localhost:3000",
+    ],
     credentials: true,
   },
 });
