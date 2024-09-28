dc34b0cc841b56801679ca7ec7abbdecfdebbaa9
Vivek Korah <lcb2021051@iiitl.ac.in>
2024-09-28 16:09:29 +0530

    trying for deployment

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const { Server } = require("socket.io");
 const app = express();
 require("dotenv").config();
 
+// CORS configuration
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
@@ -22,25 +23,16 @@ app.use(express.json());
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoutes);
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Backend is here");
 });
 
-// app.options("*", cors()); // Handle preflight requests for all routes
-app.options("*", (req, res) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    req.headers.origin || process.env.FRONTEND_URL
-  );
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200); // Always return 200 for preflight
-});
+// Handle preflight requests
+app.options("*", cors()); // Let CORS middleware handle OPTIONS requests
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("DB Connected Succesfully");
+    console.log("DB Connected Successfully");
   })
   .catch((err) => {
     console.log(`Error : ${err.message}`);
@@ -50,9 +42,12 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log(`Server started at Port : ${process.env.PORT}`);
 });
 
+// Socket.io configuration
 const io = new Server(server, {
-  cors: { origin: process.env.FRONTEND_URL },
-  credentials: true,
+  cors: {
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    credentials: true,
+  },
 });
 
 global.onlineUsers = new Map();
@@ -65,7 +60,7 @@ io.on("connection", (socket) => {
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+      socket.to(sendUserSocket).emit("msg-receive", data.message);
     }
   });
 });
